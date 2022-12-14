1bf08ffd49aa0e81088f77c4ae55251e70a9160c
Maggie Abernathy <mlabernathy05@gmail.com>
2022-12-14 15:29:58 -0600

    moved git ignore to file instead of folder... lost hours...

--- develop/index.js
+++ develop/index.js
@@ -1,5 +1,9 @@
+// const fs = require('fs');
+// const path = require('path');
+// const inquirer = require('inquirer');
 import inquirer from 'inquirer';
 import fs from 'fs';
+
 
 inquirer
   .prompt([
@@ -72,3 +76,55 @@ inquirer
       err ? console.log(err) : console.log('Successfully created README.md!')
     );
   });
+
+  let generateREADME = ({ project_name, purpose, installation, usage, contributorsHuman, contributors3rd, licenseOptions, contributing, testing, roadMap, email, gitHub}) =>
+  `# ${project_name}
+  
+  <img width="100" alt="badge for licensure" src="https://img.shields.io/badge/License-${licenseOptions}-blue.svg">
+  
+  ## Table of Contents:
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Contribute](#contributing-to-this-project)
+  - [Testing](#testing-methods)
+  - [Road_Map](#road-map)
+  - [Questions](#questions)
+  - [License](#license)
+  
+  ## Description
+  ${purpose}
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  ## Credits
+  Individual Collaborators:
+  ${contributorsHuman}
+  
+  Third Part Contributors:
+  ${contributors3rd}
+  
+  ## Contributing to this Project
+  ${contributing}
+  
+  ## Testing Methods
+  ${testing}
+  
+  ## Road Map
+  ${roadMap}
+  
+  ## Questions
+  For support, Contact Me: ${email}
+  GitHub User Profile: www.github.com/${gitHub}
+  
+  ## License
+  Licensed under the ${licenseOptions}.
+  
+    `
+  ;
